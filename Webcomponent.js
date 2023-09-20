b844ada47c1da36ace72f54367641f79d72ee084
apaccagnella <145574223+apaccagnella@users.noreply.github.com>
2023-09-20 16:14:53 -0400

    Update Webcomponent.js

--- Webcomponent.js
+++ Webcomponent.js
@@ -31,18 +31,10 @@
                 'x-csrf-token': 'fetch',} })
         .then(response => {
            var csrfToken = response.headers.get('x-csrf-token');
-            console.log('CSRF token:', csrfToken);
-             if (messagesElement) {
-           messagesElement.textContent = '';  // Clear the messages
-          messagesElement.textContent += 'CSRF token: ' + csrfToken + '\n';}
+            console.log('CSRF token:', csrfToken);          
 
-        })
-        .catch(error => console.error('Error:', error));
-  	};    
-            
-               
-        
+        })}; 
     }
 
-    customElements.define('custom-button', PerformanceHelp);
+ //   customElements.define('custom-button', PerformanceHelp);
 })();
